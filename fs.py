718ac6f14f84af257cc54069aa537f4931da92f1
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2024-01-14 23:34:15 +0800

    cleaned code and removed testing code

--- fs.py
+++ fs.py
@@ -5,35 +5,24 @@ import os
 
 load_dotenv()
 
-def mkdirSamba(remote_url, folder_name, samba_username):
+def mkdirSamba(folder_name):
+    """Create a Directory on the OptiSafe File Server, folder name may also be an address"""
     # Construct the smbclient command
-    command = f"smbclient {remote_url} --user {samba_username} -c 'mkdir {folder_name}'"
+    command = f"smbclient {os.getenv('FS_SERVER')} --user {os.getenv('FS_USER_PASSWORD')} -c 'mkdir {folder_name}'"
     try:
-        print(command)
         process = subprocess.Popen(command, shell=True)
         process.wait()
-        print(f"Folder '{folder_name}' successfully created on Samba server at '{remote_samba_path}'.")
+        print(f"Folder '{folder_name}' successfully created.")
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
 
-def putSamba(remote_url, samba_username, local_file, dir_and_name):
+def putSamba(local_file, dir_and_name):
+    """Uploads a File from the local Machine to the OptiSafe File Server"""
     # Construct the smbclient command
-    command = f"smbclient {remote_url} --user {samba_username} -c 'put {local_file} {dir_and_name}'"
+    command = f"smbclient {os.getenv('FS_SERVER')} --user {os.getenv('FS_USER_PASSWORD')} -c 'put {local_file} {dir_and_name}'"
     try:
-        print(command)
         process = subprocess.Popen(command, shell=True)
         process.wait()
         print(f"File '{dir_and_name}' successfully moved to '{dir_and_name}'.")
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
-
-if __name__ == "__main__":
-    # Replace these variables with your actual values for testing
-    remote_samba_path = os.getenv('FS_SERVER')
-    folder_name = "testa"
-    username = os.getenv('FS_USER_PASSWORD')
-    local_file_path = "/home/angelorecio/Documents/VSCODE/OptiSafe_Camera_System/testFile.txt"
-    filename = f"testa/testFile.txt"
-
-    #mkdirSamba(remote_samba_path, folder_name, username)
-    putSamba(remote_samba_path, username, local_file_path, filename)
