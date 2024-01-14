5d2f955e6d54eb1b6f663c3e09f77001e31868b7
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2024-01-14 23:33:32 +0800

    removed unused custom exception

--- customExcepts.py
+++
@@ -1,4 +0,0 @@
-class UnequalArrayLengthException(Exception):
-    def __init__(self, message="Arrays must have the same length"):
-        self.message = message
-        super().__init__(self.message)
