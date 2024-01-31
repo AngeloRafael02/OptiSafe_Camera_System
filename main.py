2233b4a993f005f470e689baf6ddb39d3270f196
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2024-01-31 16:30:29 +0800

    added cleanup function when daemon iis stopped

--- main.py
+++ main.py
@@ -49,20 +49,23 @@ def generate_frames():
           
 def take_screenshot(results):
     '''Takes a Screenshot and saves it to a file server and its metadata in a database'''
-
     #Setting up screenshot and metadata 
     hostname = socket.gethostname()
-    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     screenshot_fileLoc = f'screenshots/{hostname}_{current_time}.jpg' #temporary local storage location
     fileName = screenshot_fileLoc[len('screenshots/'):-len('.jpg')]
-
+    
     #Create an array storing the frequencies of objects, 
     classArray = results[0].boxes.cls.numpy().copy()
-
+    
     # Temporarily stores screenshot to local directory
     cv2.imwrite(screenshot_fileLoc, results[0].plot())
-    newURL = f'{datetime.now().strftime("%Y-%m-%d")}/{hostname}'
-
+    newURL = f'{datetime.datetime.now().strftime("%Y-%m-%d")}/{hostname}'
+   
+    # Make Directory/Path if not Found
+    if fs.checkDir(currentFolder) == False:
+        fs.mkdirSamba(currentFolder)
+        time.sleep(1)
     # Add screenshot to File Server
     fs.putSamba(screenshot_fileLoc, f'{newURL}/{screenshot_fileLoc}')
     
@@ -70,9 +73,19 @@ def take_screenshot(results):
     for value in classArray:
         db.upload_metadata(fileName, newURL, os.getenv('HOSTNAME_ID'), value)
     
-    # Delete Photos from temp directory 
+    # Delete Excess Photos from temp directory 
     try:
         os.remove(screenshot_fileLoc) #Delete Screenshot in local machine Permanently
+        empty_temp()
+    except FileNotFoundError:
+        print(f"File '{screenshot_fileLoc}' not found. Skipping removal.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def empty_temp():
+    """Removes any pictures in temporary folder"""
+    try:
         folder_name = "screenshots"
         folder_path = os.path.join(os.path.dirname(__file__), folder_name)
         file_list = os.listdir(folder_path)
@@ -81,7 +94,19 @@ def take_screenshot(results):
             if os.path.isfile(file_path):
                 os.remove(file_path)
                 print(f"Removed: {file_path}")
-    except FileNotFoundError:
-        print(f"File '{screenshot_fileLoc}' not found. Skipping removal.")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+
+def cleanup():
+    empty_temp()
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, cleanup)
+    currentFolder = f'{datetime.datetime.now().strftime("%Y-%m-%d")}/{socket.gethostname()}'
+    try:
+        generate_frames()
+    except KeyboardInterrupt:
+        pass
